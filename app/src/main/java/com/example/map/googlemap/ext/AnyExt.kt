1321c1a77e4d87f1ae45880cd72dc1f1777fabd2
Ксения Ладошко <87228073+KsushaLad@users.noreply.github.com>
2022-01-06 12:15:56 +0200

    Delete app/src/main/java/com/example/map/googlemap/ext directory

--- app/src/main/java/com/example/map/googlemap/ext/AnyExt.kt
+++
@@ -1,5 +0,0 @@
-package com.example.map.googlemap.ext
-
-import com.google.gson.Gson
-
-inline fun Any.toJsonString(): String = Gson().toJson(this)
--- app/src/main/java/com/example/map/googlemap/ext/LatLngExt.kt
+++
@@ -1,9 +0,0 @@
-package com.example.map.googlemap.ext
-
-import com.google.android.gms.maps.model.LatLng
-
-fun LatLng.toParam(): String {
-    val lat: Double = latitude
-    val lng: Double = longitude
-    return StringBuilder(60).append(lat).append(",").append(lng).toString()
-}
